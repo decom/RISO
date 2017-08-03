47a8581b75a283ba9f30dffbc4c44a77bfc26888
Alain André Tomaz Amaral <alainandreamaral@gmail.com>
2017-08-03 11:56:40 -0300

    Correção na configuração do grub

--- install.sh
+++ install.sh
@@ -49,8 +49,8 @@ instalar() {
     echo 'GRUB_TIMEOUT=-1' >> /etc/default/grub
     sed /'GRUB_DISABLE_LINUX_UUID='/d -i /etc/default/grub
     echo 'GRUB_DISABLE_LINUX_UUID=true' >> /etc/default/grub
-    sed /'GRUB_DISABLE_LINUX_RECOVERY='/d -i /etc/default/grub
-    echo 'GRUB_DISABLE_LINUX_RECOVERY="true"' >> /etc/default/grub
+    sed /'GRUB_DISABLE_RECOVERY='/d -i /etc/default/grub
+    echo 'GRUB_DISABLE_RECOVERY="true"' >> /etc/default/grub
     rm -f /etc/grub.d/20_memtest86+
     if [ -e /etc/grub.d/10_linux ]; then
         mv /etc/grub.d/10_linux /etc/grub.d/50_linux
